485de694f1fd53886766747bcd630d722f8449aa
Martin Smith <martin.smifff@gmail.com>
2021-01-03 09:53:05 +0000

    Lock down chat and check messages for bad words

--- functions/chat/index.js
+++ functions/chat/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const Filter = require('bad-words');
 
 exports.post = functions.https.onCall(async (data, context) => {
   if (!context.auth) {
@@ -38,4 +39,19 @@ exports.cleanMessages = functions.firestore
     var batch = admin.firestore().batch();
     msgSnap.forEach(doc => batch.delete(doc.ref));
     return batch.commit();
+  });
+
+exports.detectEvilUser = functions.firestore
+  .document('messages/{messageId}')
+  .onCreate(async (doc, context) => {
+    const filter = new Filter();
+    const { message, userId } = doc.data();
+
+    if (filter.isProfane(message)) {
+      console.log('Detected bad language');
+      await doc.ref.update({
+        message: '🤐 This message has been removed due to bad language',
+      });
+    }
+    return null;
   });
